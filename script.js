2a1f486f2481c553912df96498476db0e9562ff7
baylagas <balbino_a@hotmail.com>
2021-01-25 23:27:20 -0600

    init files

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage(message)
+{
+    /* crear un mensaje en pantalla que reciba una cadena message y concatenarlo con */
+    /* el nombre balbino */
+    var teacherName = "balbino";
+    var finalMessage = message + teacherName
+    alert(finalMessage);
+}
